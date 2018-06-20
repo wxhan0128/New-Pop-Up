fbea8d76b397da27c0c603cd75de547d3b0fba3c
wenxhan <wenxhan@iu.edu>
2018-06-20 01:18:11 -0700

    solve the display and hidden problem

--- src/main/resources/static/js/cust/test.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	// GET REQUEST
-	$(".commentBt").click(function(event) {
-		var message = [[${post.id}]];
-		console.log(message);
-		$(".commentArea").toggle();
-	});
-});
